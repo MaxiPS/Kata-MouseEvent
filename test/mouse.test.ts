df2c076e46f6d95f564b9672976db74924ec69e5
MaximilianoSpaliansky <usr2254@salesianos-lacuesta.net>
2022-05-02 12:45:17 +0100

    detect clicks when mouse is clicked - red

--- test/mouse.test.ts
+++ test/mouse.test.ts
@@ -1,4 +1,30 @@
+import { Coordinates } from "../src/coordinates";
+import { EventType } from "../src/event-type";
+import { Listener } from "../src/listener";
+import { Mouse } from "../src/mouse";
 
-describe("Mouse Listener Should", () => {
-    
+class MockListener implements Listener {
+  lastEvent: EventType;
+  handleMouseEvent(eventType: EventType) {
+    this.lastEvent = eventType;
+  }
+}
+
+describe("Mouse Should", () => {
+  let mouse: Mouse;
+  let mockListener: MockListener;
+
+  beforeEach(() => {
+    mouse = new Mouse();
+    mockListener = new MockListener();
+    mouse.subscribe(mockListener);
+  });
+
+  it("detect clicks when mouse is clicked", () => {
+    mouse.pressLeftButton(200);
+    mouse.releaseLeftButton(400);
+
+    expect(mockListener.lastEvent).toBe(EventType.Click);
+  });
+  
 });
